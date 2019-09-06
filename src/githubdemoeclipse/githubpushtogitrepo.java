6163aa3821beb2e0df6d63acbe77a3d94673c4ce
vinnie <vinniearora11@gmail.com>
2019-09-06 23:51:59 +1000

    demo of git

---
+++ src/githubdemoeclipse/githubpushtogitrepo.java
@@ -0,0 +1,28 @@
+package githubdemoeclipse;
+
+public class githubpushtogitrepo {
+ 
+	
+	
+	public static void main(String[] args) {
+		
+		githubpushtogitrepo obj=new githubpushtogitrepo();
+		obj.add();
+	}
+    
+	
+	
+	
+	public void add()
+	{
+		int x=10;
+		int y=30;
+		System.out.println("This prgram adds two number");
+		int c=x+y;
+		System.out.println("The ans is"+" " + c );
+		
+	}
+	
+	
+	
+}
